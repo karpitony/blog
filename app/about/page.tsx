8452f5e9c48557bd3c046ce5e2b6cf7bc9a69579
Yunseok <watemark1017@gmail.com>
2024-10-23 01:05:54 +0900

    feat: NavBar에 소개 페이지 추가

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default async function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Hi, I'm a developer.</p>
+    </div>
+  );
+}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,21 +9,21 @@ export default function NavBar() {
           <div className="flex items-center">
             <Link href="/" className="flex items-center">
               <SpinningReactSm />
-              <span className="font-bold text-lg md:text-xl">개발 블로그</span>
+              <p className="font-bold text-lg md:text-xl">개발 블로그</p>
             </Link>
           </div>
           <div className="flex items-center text-base">
             <Link
-              href="/"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
-            >
-              홈
-            </Link>
-            <Link
               href="/posts"
               className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium ml-0 md:ml-4"
             >
-              블로그
+              포스트
+            </Link>
+            <Link
+              href="/about"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium ml-0 md:ml-4"
+            >
+              와타시
             </Link>
           </div>
         </div>
