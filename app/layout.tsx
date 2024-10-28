fda9521d9efab6a1c32114468ea23dc06d462f8f
Yunseok <watemark1017@gmail.com>
2024-10-28 14:59:49 +0900

    style: 헤더 스타일 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className="bg-gradient-to-br from-blue-950 to-gray-900">
         <SimpleNavBar />
-        <div className="min-h-[85vh] text-gray-100 flex flex-col items-center p-3 md:p-6">
+        <div className="min-h-[80vh] text-gray-100 flex flex-col items-center p-3 md:p-6">
           {children}
         </div>
         <Footer />
--- components/SimpleNavBar.tsx
+++ components/SimpleNavBar.tsx
@@ -1,29 +1,36 @@
 import Link from 'next/link';
 import { SpinningReactSm } from './SpinningReactLogo';
+import cn from '@yeahx4/cn';
+
+const LinkData = [
+  { href: '/posts', label: '글 목록' },
+  { href: '/tags', label: '태그' },
+  { href: '/about', label: '소개' },
+];
 
 export default function NavBar() {
   return (
     <nav className="text-white max-w-3xl mx-auto border-b-2">
-      <div className="flex items-center justify-between py-4">
+      <div className="flex items-center justify-between py-6">
         <div className="flex items-center">
           <Link href="/" className="flex items-center">
             <SpinningReactSm />
             <p className="font-bold text-lg md:text-xl">개발 블로그</p>
           </Link>
         </div>
-        <div className="flex items-center text-base">
+        <div className="flex items-center">
+        {LinkData.map(({ href, label }) => (
           <Link
-            href="/posts"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium ml-0 md:ml-4"
+            href={href}
+            className={cn(
+              "text-gray-300 px-3 py-2 font-bold text-base rounded-md ml-0 md:ml-4",
+              "hover:text-transparent bg-clip-text hover:bg-gradient-to-r hover:from-blue-300 hover:to-teal-400"
+            )}
+            key={href}
           >
-            포스트
+            {label}
           </Link>
-          <Link
-            href="/about"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium ml-0 md:ml-4"
-          >
-            와타시
-          </Link>
+        ))}
         </div>
       </div>
     </nav>
