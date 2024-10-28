933a0fea9d9febbcd56ed73e7aa87719f0e01267
Yunseok <watemark1017@gmail.com>
2024-10-28 13:39:19 +0900

    feat: ToC 추가

--- app/(blog)/posts/[...slugs]/page.tsx
+++ app/(blog)/posts/[...slugs]/page.tsx
@@ -5,6 +5,7 @@ import { parsePost, PostMeta } from '@/libs/Post/PostMetadataParser';
 import { FaHome } from "react-icons/fa";
 import PostInfoHeader from "@/components/PostsPage/PostInfoHeader";
 import MarkdownRender from "@/components/MarkdownRender/MarkdownRender";
+import TableOfContent from '@/components/MarkdownRender/TableOfContent';
 
 interface PostPageProps {
   params: {
@@ -51,18 +52,23 @@ export default async function PostPage({ params: { slugs }}: PostPageProps) {
           href="/posts" 
           className="inline-flex items-center text-blue-400 hover:text-blue-300 transition-colors duration-200 mb-3 md:mb-6"
         >
-          <FaHome className='mr-2 w-6 h-6'/>
+          <FaHome className="mr-2 w-6 h-6"/>
           Back to Posts
         </Link>
+        {/* Post Info and Article */}
         <div>
           <PostInfoHeader meta={meta} />
         </div>
         <div className="bg-gray-900 bg-opacity-50 rounded-lg p-4 md:p-8 shadow-lg border border-gray-700 md:border-none mt-4">
-          { /* 게시글 내용 */ }
           <div className="markdown-body bg-transparent text-gray-100">
             <MarkdownRender markdownText={body.join("\n")} />
           </div>
         </div>
+      </div>
+      
+      {/* Table of Contents */}
+      <div className="hidden 2xl:block fixed top-1/2 right-8 transform -translate-y-1/2">
+        <TableOfContent content={body.join("\n")} />
       </div>
     </>
   );
