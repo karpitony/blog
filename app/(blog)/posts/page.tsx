be4d5447ca294f8c24a8def896385d904b3a2d69
Yunseok <watemark1017@gmail.com>
2024-10-30 15:39:53 +0900

    refactor: 컴포넌트를 밖에서 더 조종할 수 있게 수정

--- app/(blog)/posts/page.tsx
+++ app/(blog)/posts/page.tsx
@@ -13,7 +13,7 @@ export default async function PostsPage() {
     <div className="w-full max-w-full md:max-w-3xl">
       <h1 className="text-3xl font-bold mb-6">Posts</h1>
       <div className="space-y-4">
-        <PostsList posts={posts} />
+        <PostsList posts={posts} postPerPage={5} showPrevNext />
       </div>
     </div>
   );
