23156182c0bee9f6c7a2076fb8f88e77dad0ec1f
Yunseok <watemark1017@gmail.com>
2024-11-04 14:28:13 +0900

    chore: 구글 서치 콘솔 + vercel 통계 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 import SimpleNavBar from "@/components/SimpleNavBar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Blog",
+  title: "개발 블로그",
   description: "개발 블로그는 사서 드세요.",
   icons: {
     icon: "/react-logo.svg",
@@ -18,12 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta name="google-site-verification" content="r766th2h5faMjIfYnPUTpD-8xpC7920UHRfOOgQKOVY" />
+      </head>
       <body className="bg-gradient-to-br from-blue-950 to-gray-900">
         <SimpleNavBar />
         <div className="min-h-[80vh] text-gray-100 flex flex-col items-center p-3 md:p-6">
           {children}
         </div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
