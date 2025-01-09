bb5cb28d353699ceb42a93e32f30cb85c56d14a4
Yunseok <watemark1017@gmail.com>
2025-01-09 15:00:17 +0900

    feat: Education 레이아웃 수정 + 아이콘 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,18 @@ import Education from "@/components/About/Education";
 
 export default async function About() {
   return (
-    <div className="w-full max-w-full md:max-w-3xl space-y-8">
-      <div className="px-2">
+    <div className="w-full max-w-full md:max-w-3xl space-y-6">
+      <div className="px-2 mb-8">
         <SimpleAboutMe />
-        <hr className="border-t-2"/>
+      </div>
+      <hr className="border-t-2"/>
+      <div className="px-2">
         <Education />
+      </div>
+      <hr className="border-t-2"/>
+      <div className="px-2">
+        {/* <Education />
+        TBD */}
       </div>
     </div>
   );
--- components/About/Education.tsx
+++ components/About/Education.tsx
@@ -1,29 +1,48 @@
+import Image from "next/image"
 
 const EducationData = [
   {
-    year: "2024.03 ~ 2025.01",
-    title: "동국대학교 경영정보학과",
-    description: "학사과정"
+    logo: "/assets/education/dongguk.ico",
+    title: "동국대학교",
+    programs: [
+      {
+        description: "경영정보학과",
+        year: "2024.03 ~ 2025.01",
+      },
+      {
+        description: "컴퓨터 • AI학부",
+        year: "2025.03 ~ 재학중",
+      }
+    ]
   },
-  {
-    year: "2025.03 ~",
-    title: "동국대학교 컴퓨터 • AI학부",
-    description: "학사과정"
-  }
 ]
 
 export default function Education() {
   return (
     <div>
-      <h2 className="text-2xl font-bold">Education</h2>
-      <div>
-        {EducationData.map((education, index) => (
-          <div key={index} className="flex flex-col md:flex-row justify-between items-start md:items-center">
-            <div className="text-lg font-bold">{education.year}</div>
-            <div className="text-lg font-bold">{education.title}</div>
-            <div className="text-lg">{education.description}</div>
+      <h2 className="text-2xl font-bold my-4">교육</h2>
+      <div className="flex flex-col items-center space-y-4">
+      {EducationData.map((education, index) => (
+        <div key={index} className="flex flex-row items-start mb-6">
+          <div className="flex items-center border-2 border-white bg-black rounded-full p-[6px]">
+            <Image 
+              src={education.logo}
+              alt={education.title}
+              width={25}
+              height={25} 
+            />
           </div>
-        ))}
+          <div className="flex flex-col items-start ml-4 w-64">
+            <div className="text-xl font-bold mb-2">{education.title}</div>
+            {education.programs.map((program, programIndex) => (
+              <div key={programIndex} className="flex flex-row justify-between items-center w-full">
+                <div className="text-base font-semibold">{program.description}</div>
+                <div className="text-sm opacity-80">{program.year}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
       </div>
     </div>
   )
