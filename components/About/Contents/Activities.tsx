14a8ca19d9ccdfeadd96b633fb96c319f2cad48a
Yunseok <watemark1017@gmail.com>
2025-03-05 22:10:15 +0900

    docs: 활동에 팜시스템, 구름톤 유니브 4기 추가

--- components/About/Contents/Activities.tsx
+++ components/About/Contents/Activities.tsx
@@ -11,6 +11,32 @@ const ActivitiyData = [
           "- 대학생 연합 IT 개발 동아리 [구름톤 유니브](https://9oormthon.university/)에서 7주동안 [리액트 스터디](https://my-react-study.vercel.app/)에 참여\n"+
           "- kakao x 구름톤 유니브 3기 연합해커톤 대상(1위) 수상. 2024.09.29\n",
         year: "2024.07 ~ 2025.01",
+      },
+      {
+        description: 
+          "### 구름톤 유니브 4기 동국대학교\n" +
+          "- ... 제 활약을 기대해주세요!",
+        year: "2025.03 ~ ",
+      }
+    ],
+  },
+  {
+    title: "팜 시스템",
+    programs: [
+      {
+        description: 
+          "### 웹페이지 제작팀\n" +
+          // 안어울리긴 한데 일단은 프로젝트보다 이쪽에 기술함
+          "[**Github**](https://github.com/DguFarmSystem/Homepage-FE) | [**링크**](https://www.farmsystem.kr)\n" +
+          "- **웹페이지 제작 팀**의 프론트엔드 파트로 참여\n" +
+          "- 코드 재사용성을 생각해 `models`, `services`, `hooks`로 API 통신 로직을 분리함",
+        year: "2025.01 ~ ",
+      },
+      {
+        description: 
+          "### 보안/웹 트랙 4기\n" +
+          "- ... 제 활약을 기대해주세요!",
+        year: "2025.03 ~ ",
       }
     ],
   }
