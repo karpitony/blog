bc7959b22d0b31872e95743602306326b1a31631
Yunseok <watemark1017@gmail.com>
2025-03-18 12:43:42 +0900

    feat: 프로젝트 모음 페이지 틀 제작

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+export default function Projects() {
+  return (
+    <div className="w-full max-w-full md:max-w-3xl">
+      <h1>나의 활동</h1>
+      <h2>KEY WORDS</h2>
+      {/* <ProjectsKeyWords /> */}
+      <h2>PROJECTS</h2>
+      {/* <ProjectsList /> */}
+    </div>
+  );
+}
