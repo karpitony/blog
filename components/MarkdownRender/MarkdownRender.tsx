ff6c985141cf0a1df879608cd03a51db123f24ea
Yunseok <watemark1017@gmail.com>
2025-03-28 23:30:58 +0900

    docs: generateStaticParams 적용기 게시

--- components/MarkdownRender/MarkdownRender.tsx
+++ components/MarkdownRender/MarkdownRender.tsx
@@ -24,8 +24,8 @@ export default function MarkdownRender({ markdownText, enableGap=true }: Markdow
         components={{
           h1: ({ ...props }) => <h1 className="text-4xl font-bold mb-4 mt-6 pt-2 text-blue-400" {...props} />,
           h2: ({ ...props }) => <h2 className="text-3xl font-semibold mb-4 mt-6 pt-2 text-blue-300" {...props} />,
-          h3: ({ ...props }) => <h3 className="text-2xl font-medium mb-3 mt-4 pt-1 text-blue-200" {...props} />,
-          h4: ({ ...props }) => <h4 className="text-xl font-medium mb-3 mt-4 pt-1 text-blue-200" {...props} />,
+          h3: ({ ...props }) => <h3 className="text-2xl font-medium mb-3 mt-4 pt-1 text-blue-300" {...props} />,
+          h4: ({ ...props }) => <h4 className="text-xl font-medium mb-3 mt-4 pt-1 text-blue-250" {...props} />,
           h5: ({ ...props }) => <h5 className="text-lg font-medium mb-3 mt-4 pt-1 text-blue-200" {...props} />,
           a: ({ href, children, ...props }) => (
             <a
