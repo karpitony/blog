fca76371bcded8a1f4f8580e02f02ebf8d6e6e5d
Yunseok <watemark1017@gmail.com>
2025-04-27 23:10:32 +0900

    feat: 프로젝트 카드 제작 + 블로그 app 구조 수정

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,24 @@
-export default function Projects() {
+import { getProjectList } from "@/libs/Project/getProjectList";
+import ProjectCard from "@/components/Projects/ProjectCard";
+
+export default async function Projects() {
+  const { projects } = await getProjectList();
   return (
     <div className="w-full max-w-full md:max-w-3xl">
       <h1>나의 활동</h1>
-      <h2>KEY WORDS</h2>
+      <h2>CATEGORY</h2>
+      <h2>TAGS</h2>
       {/* <ProjectsKeyWords /> */}
       <h2>PROJECTS</h2>
-      {/* <ProjectsList /> */}
+      <div className="mt-12 flex flex-col gap-8 md:grid md:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.slug}
+            slug={project.slug}
+            meta={project.meta}
+          />
+        ))}
+      </div>
     </div>
   );
 }
