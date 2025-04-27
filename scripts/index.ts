a42843957b35ce7bf8298afe48088ad5cf325378
Yunseok <watemark1017@gmail.com>
2025-04-27 23:07:20 +0900

    feat: 프로젝트 목록 스크립트 추가 + 스크립트 개편
    - 프로젝트 목록 스크립트 추가
    - 프로젝트 이미지도 정보 추출
    - 스크립트 구조 개편

---
+++ scripts/index.ts
@@ -0,0 +1,21 @@
+import generatePostAndDiaryList from './generate-post-list';
+import generateProjectList from './generate-project-list';
+import copyOnlyImages from './copy-assets';
+import generateImageInfo from './generate-image-info';
+
+async function main() {
+  await Promise.all([
+    generatePostAndDiaryList,
+    generateProjectList,
+    copyOnlyImages,
+    generateImageInfo,
+  ]);
+}
+
+export default main()
+  .then(() => console.log('✅ 모든 작업 완료'))
+  .catch((err) => {
+    console.error('❌ 작업 중 오류 발생:', err);
+    process.exit(1);
+  }
+);
